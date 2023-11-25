a2c469f9fa20cfc2a79a0af8e5c217a3dc3dc8d2
Dawood-Amir <dawoodamir115@gmail.com>
2023-11-25 20:47:21 +0100

    added sqlfile

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine , types
 import os
 
 
@@ -8,17 +8,15 @@ df = pd.read_csv(csv_url, delimiter=';')
 
 # Droping the "Status" column
 df = df.drop(columns=["Status"])
-
 # Convert "Laenge" and "Breite" columns to numeric, replacing commas with dots
 df["Laenge"] = pd.to_numeric(df["Laenge"].str.replace(',', '.'), errors="coerce")
 df["Breite"] = pd.to_numeric(df["Breite"].str.replace(',', '.'), errors="coerce")
 
-# Filter rows based on conditions
 df = df[df["Verkehr"].isin(["FV", "RV", "nur DPN"])]
 df = df[(df["Laenge"].notnull()) & (df["Breite"].notnull())]
 df = df[(df["Laenge"] >= -90) & (df["Laenge"] <= 90) & (df["Breite"] >= -90) & (df["Breite"] <= 90)]
 
-# Define a function to check the validity of "IFOPT" values
+# validity of IFOPT  values
 def is_valid_ifopt(value):
     if isinstance(value, str):
         parts = value.split(":")
@@ -27,24 +25,33 @@ def is_valid_ifopt(value):
     return False
 
 
-# Filter rows based on the validity of "IFOPT" values
 df = df[df["IFOPT"].apply(is_valid_ifopt)]
+df = df.dropna()
 
-# Define SQLite data types
+# Define SQLite types for each column
 sqlite_types = {
-    "EVA_NR": "BIGINT",
-    "DS100": "TEXT",
-    "IFOPT": "TEXT",
-    "NAME": "TEXT",
-    "Verkehr": "TEXT",
-    "Laenge": "FLOAT",
-    "Breite": "FLOAT",
-    "Betreiber_Name": "TEXT",
-    "Betreiber_Nr": "TEXT"
+    "EVA_NR": types.BIGINT(),
+    "DS100": types.TEXT(),
+    "IFOPT": types.TEXT(),
+    "NAME": types.TEXT(),
+    "Verkehr": types.TEXT(),
+    "Laenge": types.FLOAT(),
+    "Breite": types.FLOAT(),
+    "Betreiber_Name": types.TEXT(),
+    "Betreiber_Nr": types.TEXT()
 }
-# Create SQLite database engine
+#engine = create_engine("sqlite:///trainstops.sqlite")
 script_directory = os.path.dirname(os.path.abspath(__file__))
 db_file_path = os.path.join(script_directory, "trainstops.sqlite")
 engine = create_engine(f"sqlite:///{db_file_path}")# Write the DataFrame to the SQLite database
-df.to_sql("trainstops", engine, index=False, if_exists="replace")
-print(f"Data has been written to {db_file_path}")
+
+df.to_sql("trainstops", engine, index=False, if_exists="replace", dtype=sqlite_types)
+
+
+
+#script_directory = os.path.dirname(os.path.abspath(__file__))
+#db_file_path = os.path.join(script_directory, "trainstops.sqlite")
+#engine = create_engine(f"sqlite:///{db_file_path}")# Write the DataFrame to the SQLite database
+#df.to_sql("trainstops", engine, index=False, if_exists="replace")
+print(f"Data is written to {db_file_path}")
+print(f"created file")
