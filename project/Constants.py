99b3bbd34e9bcc7c1cd27feaab4a3dffce040719
Dawood-Amir <dawoodamir115@gmail.com>
2023-12-17 15:33:07 +0100

    added constants file

---
+++ project/Constants.py
@@ -0,0 +1,5 @@
+# add this in .gitgnore to ignore this file 
+#/project/Constants.py
+URL_Temprature = "https://query.data.world/s/43ofhgffvrdjkfx577ih5j6prp4rdr?dws=00000"
+URL_C02_Emission = "https://raw.githubusercontent.com/Dawood-Amir/Co2-Emission-Dataset/main/tidy_format_co2_emission_dataset.csv"
+urls = [URL_Temprature , URL_C02_Emission]
